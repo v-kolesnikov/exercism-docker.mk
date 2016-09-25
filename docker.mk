6267e3d82a4b6fc4747430d4b0c64b46fe5e69f0
V.Kolesnikov <re.vkolesnikov@gmail.com>
2016-09-25 13:54:42 +0300

    Add EXERCISM_CLI_VERSION arg to build command

--- docker.mk
+++ docker.mk
@@ -3,8 +3,13 @@ TAG := $(shell basename $(CURDIR))
 VOLUME := $(CURDIR):$(HOME)
 RUN := run --rm -v $(VOLUME)
 
+EXERCISM_CLI_VERSION := 2.3.0
+
 build:
-	docker build -t $(TAG) --build-arg EXERCISM_KEY=$(EXERCISM_KEY) .
+	docker build -t $(TAG) \
+		--build-arg EXERCISM_CLI_VERSION=$(EXERCISM_CLI_VERSION) \
+		--build-arg EXERCISM_KEY=$(EXERCISM_KEY) \
+		.
 
 bash:
 	docker $(RUN) -it $(TAG) bash
