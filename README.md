5aadd69f07b11748adba6c573fbc1a19368802db
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2016-11-04 22:56:46 +0300

    Refactoring project
     - Set test as default target
     - Add extensible commands: install, test and clean
     - Add generic DOCKER_RUN command

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Exercism
+# Exercism docker.mk
+
+## Usage
+
+Add to your Makefile follow lines:
+
+```makefile
+EXERCISM_LANG := %language%
+include %path to docker.mk%
+```
+
+Now you can make some exercism cli commands from docker:
+
+- `fetch`: fetch next uncomplete exercixse for EXERCISM_LANG
+- `status`: show your status on exercism.io by EXERCISM_LANG
+- `submit EXERCISE=%path to exercise file%`: submit EXERCISE to exercism.io
--- docker.mk
+++ docker.mk
@@ -1,34 +1,41 @@
-HOME := '/root/exercism'
+.DEFAULT_GOAL := test
+
+HOME := /root/exercism
 TAG := $(shell basename $(CURDIR))
 VOLUME := $(CURDIR):$(HOME)
 RUN := run --rm -v $(VOLUME)
+DOCKER_RUN := docker $(RUN) $(TAG)
 
 EXERCISM_CLI_VERSION := 2.3.0
 
+# Project API
+install:: build
+
+clean:: rm
+
+test:: ;
+
+# Docker API
 build:
 	docker build -t $(TAG) \
 		--build-arg EXERCISM_CLI_VERSION=$(EXERCISM_CLI_VERSION) \
 		--build-arg EXERCISM_KEY=$(EXERCISM_KEY) \
 		.
 
+rm:
+	docker rmi $(TAG)
+
 bash:
 	docker $(RUN) -it $(TAG) bash
 
-clean:
-	docker rmi $(TAG)
-
+# Exercism cli API
 fetch:
-	docker $(RUN) $(TAG) exercism fetch $(EXERCISM_LANG)
+	$(DOCKER_RUN) exercism fetch $(EXERCISM_LANG)
 
 submit:
-	docker $(RUN) $(TAG) exercism submit lib/exercism/$(EXERCISM_LANG)/${EXERCISE}
+	$(DOCKER_RUN) exercism submit lib/exercism/$(EXERCISM_LANG)/${EXERCISE}
 
 status:
-	docker $(RUN) $(TAG) exercism status $(EXERCISM_LANG)
-
-test:
-	docker $(RUN) $(TAG) $(TEST_COMMAND)
-
-all: build test
+	$(DOCKER_RUN) exercism status $(EXERCISM_LANG)
 
 .PHONY: test
